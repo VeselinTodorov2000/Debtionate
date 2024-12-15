dcfa91bc1b5e7e3dc49d15f95083dd2771bebdb1
VeselinTodorov2000 <veselin.todorov@dreamix.eu>
2024-12-15 13:31:55 +0200

    [Debtionate-0001] - add backend project

---
+++ backend/Deptionate/src/main/java/com/example/deptionate/DeptionateApplication.java
@@ -0,0 +1,13 @@
+package com.example.deptionate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptionateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptionateApplication.class, args);
+    }
+
+}
